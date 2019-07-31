1b01aae9e295844425a8a5743e45bd8bb8ce2415
enrico76 <enricodip@gmail.com>
2019-07-31 09:14:10 +0200

    added template handlebars

--- myscript.js
+++ myscript.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
 
+  var Booleandisc = "https://flynn.boolean.careers/exercises/api/array/music";
+  var discArr = [];
+
+  $.ajax(
+    {
+      url : Booleandisc,
+      method : "GET",
+      success : function(data) {
+
+        var disc = data.response;
+        console.log(disc);
+      }
+
+  })
+
+});
