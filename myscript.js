cbc2d7b2e9a4480841aff0f0f4a6e6f2f3d16ce5
enrico76 <enricodip@gmail.com>
2019-07-31 09:29:09 +0200

    added call ajax at api and public response on html handlebars template

--- myscript.js
+++ myscript.js
@@ -9,8 +9,33 @@ $(document).ready(function() {
       method : "GET",
       success : function(data) {
 
-        var disc = data.response;
-        console.log(disc);
+        var compilation = data.response;
+        console.log(compilation);
+
+        for (var i = 0; i < compilation.length; i++) {
+
+          var disc = compilation[i];
+          console.log(disc);
+
+          var source = $("#template").html();
+          var template = Handlebars.compile(source);
+
+          var context = {
+            poster : disc.poster,
+            title : disc.title,
+            author : disc.author,
+            year : disc.year
+          };
+          var html = template(context);
+
+          $(".cds-container").append(html);
+
+        }
+
+
+      },
+      error : function(){
+        alert("no connection");
       }
 
   })
